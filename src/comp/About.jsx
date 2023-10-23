97a568bafdca9e031cab5d3457a088e00e9be386
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-10-23 02:00:56 +0100

    section in about

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -19,6 +19,38 @@ const About = () => {
           <img src="/img-group.png" alt="" />
         </div>
       </div>
+      <div className=" md:w-[90%] m-auto mt-5">
+        <h1 className="text-center text-[25px] font-Inter font-bold mt-9 mb-9">Why Choose Us?</h1>
+        <div className="flex flex-wrap gap-5  font-Inter md:w-[80%] md:m-auto justify-center">
+          <div className="flex flex-col gap-2 max-w-[250px] bg-[#F9F8FF] rounded-2xl pt-[60px] p-[20px]  items-center">
+            <div className="h-[40px] w-[40px] bg-white rounded-md p-[10px]">
+              <img src="ecommerce.svg" alt="" />
+            </div>
+            <h1 className="text-[20px] font-bold">E-Commerce Based</h1>
+            <p>
+              Helping you sell stuff is what gets us uo in the morning - we do not do any other type of marketing.
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 max-w-[250px] bg-white rounded-2xl pt-[60px] p-[20px]  items-center border-2">
+            <div className="h-[40px] w-[40px] bg-[#F9F8FF] rounded-md p-[10px]">
+              <img src="growth.svg" alt="" />
+            </div>
+            <h1 className="text-[20px] font-bold">Focus is on Growth</h1>
+            <p>
+              Helping you sell stuff is what gets us uo in the morning - we dont do any other type of marketing.
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 max-w-[250px] bg-white rounded-2xl pt-[60px] p-[20px]  items-center border-2">
+            <div className="h-[40px] w-[40px] bg-[#F9F8FF] rounded-md p-[10px]">
+              <img src="people.svg" alt="" />
+            </div>
+            <h1 className="text-[20px] font-bold">We are Experts</h1>
+            <p>
+              Helping you sell stuff is what gets us uo in the morning - we do not do any other type of marketing.
+            </p>
+          </div>
+        </div>
+      </div>
       {/* <Footer /> */}
     </div>
   );
