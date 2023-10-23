6240d7b5b7e810f2ed4f56ab1c7fe886232758a5
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-10-23 14:18:39 +0100

    Service updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import About from "../src/comp/About";
+import Services from "./comp/Services";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services/>} />
     </Routes>
   );
 }
