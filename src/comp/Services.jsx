f4c11964a5f288ade5ddc971f9738962493b894a
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-10-23 14:31:49 +0100

    service updating

--- src/comp/Services.jsx
+++ src/comp/Services.jsx
@@ -1,7 +1,44 @@
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <>
+    <div className="md:w-[90%] m-auto">
+        <div className=" ">
+            <h1 className="text-[40px] font-bold font-Inter">
+                Our Service
+            </h1>
+            <p className="text-[1rem] font-Inter">
+            YARA is a customizable open-source eCommerce platform, empowering businesses of all sizes to create and scale their online stores. At YARA, we specialize in providing comprehensive eCommerce services tailored to meet your specific business needs. Our team of experts is dedicated to delivering the following services:
+            </p>
+        </div>
+        <ul className="font-Inter mt-5">
+            <li className="">
+                <h1 className="flex items-center gap-4 text-[1.2rem] font-bold"><div className="bg-black w-[8px] h-[8px] rounded-full"></div> Setup and Configuration</h1>
+                <p className="mt-4">
+                Our team of experienced developers will help you set up and configure your eCommerce store to ensure a seamless and efficient online shopping experience for your customers. From installation to customization, we've got you covered.
+                </p>
+            </li>
+            <li className="mt-5">
+                <h1 className="flex items-center gap-4 text-[1.2rem] font-bold"><div className="bg-black w-[8px] h-[8px] rounded-full"></div> Custom Theme Development</h1>
+                <p className="mt-4">
+                Looking for a unique and engaging design for your online store? Our expert designers will work closely with you to develop a custom eCommerce theme that reflects your brand identity and resonates with your target audience.
+                </p>
+            </li>
+            <li className="mt-5">
+                <h1 className="flex items-center gap-4 text-[1.2rem] font-bold"><div className="bg-black w-[8px] h-[8px] rounded-full"></div> Plugin Integration and Customization</h1>
+                <p className="mt-4">
+                Enhance the functionality of your eCommerce store with our plugin integration and customization services. Whether you need to integrate a payment gateway, improve security features, or add new functionalities, our team will ensure a smooth integration process.
+                </p>
+            </li>
+            <li className="mt-5">
+                <h1 className="flex items-center gap-4 text-[1.2rem] font-bold"><div className="bg-black w-[8px] h-[8px] rounded-full"></div> Product Management and Optimization</h1>
+                <p className="mt-4">
+                Optimize your product listings for maximum visibility and conversion. Our product management services include optimizing product descriptions, images, and pricing to create an appealing and user-friendly shopping experience.
+                </p>
+            </li>
+        </ul>
+    </div>
+    </>
   )
 }
 
