64f798f4352865bf4518cae0c600824e0d00f70a
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-10-27 23:36:25 +0100

    update mobile ui

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div>
       <Header />
       {/*  bg-gradient-to-t from-blue-50 via-blue-100 to-blue-400 w-64 */}
-      <div className="md:flex md:w-[90%] m-auto mt-5 gap-2">
+      <div className="p-4 md:flex md:w-[90%] m-auto mt-5 gap-2">
         <div className="w-[100%] md:w-[80%] lg:w-[60%]">
           <h1 className=" font-Inter font-bold text-[30px]">The Mission</h1>
           <p className="mt-3 font-Inter text-[.9rem] lg:text-[1.5rem]">We are a dedicated team of e-commerce enthusiasts committed to providing a seamless online shopping experience for our valued customers. With a passion for technology and a deep understanding of customer needs, we have curated a diverse collection of products designed to cater to a wide range of tastes and preferences.</p>
